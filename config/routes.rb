143fb92f1ec0b140a188cc823dc77432bb9a1c5a
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-11-17 10:17:10 +0000

    [Finish] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   get 'lists/new'
+
   post 'lists' => 'lists#create'
+
   # get 'lists/index'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+
+  # get 'lists/show' # 下記.../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
+
   get 'lists/edit'
+
   get '/top' => 'homes#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
