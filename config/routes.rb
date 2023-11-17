2cc7a3a15dec7a755cc1e9d0f44abd9b0a4ae78d
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-11-17 17:23:03 +0000

    [Finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-  get 'lists/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # get 'lists/new'
   get '/top' => 'homes#top'
-
-  post 'lists' => 'lists#create'
-
-  # get 'lists/index'
-  get 'lists' => 'lists#index'
-
-  # get 'lists/show' # 下記.../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-
-  # get 'lists/edit'
-  get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
-
-  patch 'lists/:id' => 'lists#update', as:'update_list'
+  # post 'lists' => 'lists#create'
+  # # get 'lists/index'
+  # get 'lists' => 'lists#index'
+  # # get 'lists/show' # 下記.../lists/1 や .../lists/3 に該当する
+  # get 'lists/:id' => 'lists#show', as: 'list'
+  # # get 'lists/edit'
+  # get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
+  # patch 'lists/:id' => 'lists#update', as:'update_list'
+  # delete 'lists/:id' => 'lists#destroy', as:'destroy_list'
+  resources :lists
 end
